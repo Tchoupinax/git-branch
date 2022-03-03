981fe534449884346742ed4b1fc1eb5cb5177b23
Tchoupinax <corentinfiloche@hotmail.fr>
2022-03-03 19:41:26 +0100

    feat: get the commiter date for each commit (#2)

--- main.go
+++ main.go
@@ -2,60 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"strconv"
-	"strings"
+	"sync"
 
+	timeago "github.com/caarlos0/timea.go"
 	"github.com/eiannone/keyboard"
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 )
-
-func isNumeric(s string) bool {
-	_, err := strconv.ParseFloat(s, 64)
-	return err == nil
-}
-
-func CheckIfError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func clearTerminal() {
-	fmt.Print("\033[H\033[2J") // Clear the terminal
-}
-
-func criteria(r *plumbing.Reference, isRemote bool, isTag bool, displayAll bool) bool {
-	if strings.Contains(string(r.Name()), "HEAD") {
-		return false
-	}
-
-	if displayAll {
-		return true
-	}
-
-	if isRemote {
-		return r.Name().IsRemote()
-	}
-
-	if isTag {
-		return r.Name().IsTag()
-	}
-
-	return !r.Name().IsRemote() && !r.Name().IsTag()
-}
-
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
 
 func main() {
 	path, _ := os.Getwd()
@@ -68,64 +24,95 @@ func main() {
 		os.Exit(1)
 	}
 
-	clearTerminal()
-	branches := []string{}
+	ClearTerminal()
+	branches := []Branch{}
 	refIter, _ := r.References()
-	refIter.ForEach(func(r *plumbing.Reference) error {
-		var displayAll = stringInSlice("-a", os.Args[1:]) || stringInSlice("--all", os.Args[1:])
-		var displayRemoteBranches = stringInSlice("-r", os.Args[1:])
-		var displayTags = stringInSlice("-t", os.Args[1:])
 
-		if criteria(r, displayRemoteBranches, displayTags, displayAll) {
-			branches = append(branches, r.Name().Short())
+	refIter.ForEach(func(r *plumbing.Reference) error {
+		var displayAll = StringInSlice("-a", os.Args[1:]) || StringInSlice("--all", os.Args[1:])
+		var displayRemoteBranches = StringInSlice("-r", os.Args[1:])
+		var displayTags = StringInSlice("-t", os.Args[1:])
+
+		if Criteria(r, displayRemoteBranches, displayTags, displayAll) {
+			branches = append(branches, Branch{name: r.Name().Short(), hash: r.Hash().String()})
 		}
 
 		return nil
 	})
 
+	var wg sync.WaitGroup
+	for i := range branches {
+		wg.Add(1)
+
+		go func(b *Branch) {
+			defer wg.Done()
+			b.GetCommitterDateFromLogs()
+		}(&branches[i])
+	}
+	wg.Wait()
+
+	ClearTerminal()
+
+	var lengthOfGreatestBranchLength = 0
+	for _, branch := range branches {
+		if len(branch.name) > lengthOfGreatestBranchLength {
+			lengthOfGreatestBranchLength = len(branch.name)
+		}
+	}
+
+	var isTheFirstArgumentIsANumber = (len(os.Args) > 1 && IsNumeric(os.Args[1])) || (len(os.Args) > 2 && (IsNumeric(os.Args[2])))
+
 	bold := color.New(color.Bold).SprintFunc()
 	yellow := color.New(color.Bold, color.FgYellow).SprintFunc()
 	blue := color.New(color.Bold, color.FgBlue).SprintFunc()
 	red := color.New(color.Italic, color.FgRed).SprintFunc()
-	fmt.Println("")
-	fmt.Println(bold("⚡️ Git branch"))
-	fmt.Println("")
+	if !isTheFirstArgumentIsANumber {
+		fmt.Println("")
+		fmt.Println(bold("⚡️ Git branch"))
+		fmt.Println("")
 
-	var count = 1
-	for _, v := range branches {
-		if count%2 == 0 {
-			fmt.Println(blue(count, "  ", v))
-		} else {
-			fmt.Println(yellow(count, "  ", v))
+		var count = 1
+		for _, branch := range branches {
+			s := timeago.Of(branch.commitedAt)
+
+			if count%2 == 0 {
+				fmt.Println(blue(count, "  ", branch.name, GenerateSpace(lengthOfGreatestBranchLength-len(branch.name)), "       (", s, ")"))
+			} else {
+				fmt.Println(yellow(count, "  ", branch.name, GenerateSpace(lengthOfGreatestBranchLength-len(branch.name)), "       (", s, ")"))
+			}
+
+			count = count + 1
 		}
-
-		count = count + 1
 	}
 
 	worktree, _ := r.Worktree()
 
-	var isTheFirstArgumentIsANumber = isNumeric(os.Args[1])
 	var desiredBranchNumber int
 	if isTheFirstArgumentIsANumber {
-		tmpNumber, _ := strconv.Atoi(os.Args[1])
+		tmpNumber, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			tmpNumber, _ = strconv.Atoi(os.Args[2])
+		}
+
 		desiredBranchNumber = tmpNumber - 1
 	} else {
 		desiredBranchNumber = chooseBranchNumber()
 	}
 
 	desiredBranch := branches[desiredBranchNumber]
-
 	error := worktree.Checkout(&git.CheckoutOptions{
-		Branch: plumbing.NewBranchReferenceName(desiredBranch),
+		Branch: plumbing.NewBranchReferenceName(desiredBranch.name),
 	})
 
-	clearTerminal()
+	ClearTerminal()
 
-	fmt.Println(red(error))
-	fmt.Println("")
+	if error != nil {
+		fmt.Println(red(error))
+		fmt.Println("")
+	}
 
 	purple := color.New(color.Bold, color.FgHiMagenta).SprintFunc()
-	fmt.Println(purple("Checkout the branch ", desiredBranch, "!"))
+	fmt.Println(purple("Checkout the branch ", desiredBranch.name, "!"))
 }
 
 func chooseBranchNumber() int {
@@ -136,7 +123,7 @@ func chooseBranchNumber() int {
 	input := read()
 	fmt.Println()
 
-	if !isNumeric(input) {
+	if !IsNumeric(input) {
 		fmt.Println()
 		red := color.New(color.Bold, color.BgHiRed).SprintFunc()
 		fmt.Printf(red("you should type a number"))
---
+++ utils.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+func IsNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+
+func CheckIfError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GenerateSpace(length int) string {
+	var str = "        "
+
+	for i := 0; i < length; i++ {
+		str += " "
+	}
+
+	return str
+}
+
+func ClearTerminal() {
+	// fmt.Print("\033[H\033[2J") // Clear the terminal
+}
+
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func Criteria(r *plumbing.Reference, isRemote bool, isTag bool, displayAll bool) bool {
+	if strings.Contains(string(r.Name()), "HEAD") {
+		return false
+	}
+
+	if displayAll {
+		return true
+	}
+
+	if isRemote {
+		return r.Name().IsRemote()
+	}
+
+	if isTag {
+		return r.Name().IsTag()
+	}
+
+	return !r.Name().IsRemote() && !r.Name().IsTag()
+}

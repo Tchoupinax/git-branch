5ca04832010454944afb3a081a911a3b4431ba70
Tchoupinax <corentinfiloche@hotmail.fr>
2023-11-26 12:00:28 +0100

    feat: add helper in version

--- version.go
+++ version.go
@@ -15,12 +15,16 @@ func cliCommandDisplayVersion(args []string) {
 
 	if displayVersion {
 		bold := color.New(color.Bold).SprintFunc()
+		italic := color.New(color.Italic).SprintFunc()
+
 		fmt.Println()
 		fmt.Println(bold("⚡️ Git branch"))
 		fmt.Println()
 		fmt.Println("build date: ", bold(BuildDate))
 		fmt.Println("version:        ", bold(Version))
 		fmt.Println()
+		fmt.Println(italic("Need help?"))
+		fmt.Println(italic("https://github.com/Tchoupinax/git-branch/issues"))
 		os.Exit(0)
 	}
 }

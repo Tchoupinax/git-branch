cee5cad5a73c08bc4c1e4de9829640445afc7576
Tchoupinax <corentinfiloche@hotmail.fr>
2023-11-26 12:05:08 +0100

    chore: upgrade version in Linux installation script

--- scripts/installation-linux.sh
+++ scripts/installation-linux.sh
@@ -10,7 +10,7 @@ function fatal() {
   exit 1
 }
 
-VERSION=0.0.10
+VERSION="0.0.11"
 
 info "Welcome to git-branch!"
 
@@ -18,16 +18,16 @@ info "Downloading version v$VERSION..."
 ARCH=$(dpkg --print-architecture)
 
 if [[ $ARCH = "amd64" ]]; then
-  curl https://github.com/Tchoupinax/git-branch/releases/download/v0.0.10/git-branch_0.0.10_linux_amd64.tar.gz \
-    -Lo /tmp/git-branch_0.0.10_linux_amd64.tar.gz 2> /dev/null
+  curl https://github.com/Tchoupinax/git-branch/releases/download/v0.0.10/git-branch_0.0.11_linux_amd64.tar.gz \
+    -Lo /tmp/git-branch_0.0.11_linux_amd64.tar.gz 2> /dev/null
 else
   fatal "Architecture $ARCH is not supported by this script for the moment, please be free to open an issue."
   exit 1
 fi
 
 info "Installing..."
-tar -xf /tmp/git-branch_0.0.10_linux_amd64.tar.gz
-rm -f /tmp/git-branch_0.0.10_linux_amd64.tar.gz
+tar -xf /tmp/git-branch_0.0.11_linux_amd64.tar.gz
+rm -f /tmp/git-branch_0.0.11_linux_amd64.tar.gz
 
 warn "Asking you password for installing in your path [/usr/local/bin]"
 sudo mv git-branch /usr/local/bin/gb

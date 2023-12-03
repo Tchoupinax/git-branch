a9fd94500e3ca3567cc448cf54306a9d56b6194c
Tchoupinax <corentinfiloche@hotmail.fr>
2023-12-03 13:48:32 +0100

    feat: repair linux script

--- scripts/installation-linux.sh
+++ scripts/installation-linux.sh
@@ -18,19 +18,24 @@ info "Downloading version v$VERSION..."
 ARCH=$(dpkg --print-architecture)
 
 if [[ $ARCH = "amd64" ]]; then
-  curl https://github.com/Tchoupinax/git-branch/releases/download/v0.0.10/git-branch_0.0.11_linux_amd64.tar.gz \
-    -Lo /tmp/git-branch_0.0.11_linux_amd64.tar.gz 2> /dev/null
+  curl https://github.com/Tchoupinax/git-branch/archive/refs/tags/v0.0.11.tar.gz \
+    -Lo /tmp/git-branch-v0.0.11.tar.gz 2> /dev/null
 else
   fatal "Architecture $ARCH is not supported by this script for the moment, please be free to open an issue."
   exit 1
 fi
 
 info "Installing..."
-tar -xf /tmp/git-branch_0.0.11_linux_amd64.tar.gz
-rm -f /tmp/git-branch_0.0.11_linux_amd64.tar.gz
+tar -xvzf /tmp/git-branch-v0.0.11.tar.gz
+rm -f /tmp/git-branch-v0.0.11.tar.gz
+
+cd git-branch-0.0.11
+info "building..."
+go build -o git-branch
 
 warn "Asking you password for installing in your path [/usr/local/bin]"
 sudo mv git-branch /usr/local/bin/gb
-rm -f git-branch
+cd ..
+rm -rf git-branch-0.0.11
 
 info "Successfully installed!"

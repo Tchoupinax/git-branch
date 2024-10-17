dacfc6132a27ea9ca075b38c5dac5f9514fe9aa5
Tchoupinax <corentinfiloche@hotmail.fr>
2024-10-17 13:31:12 +0200

    chore: prepare release 0.3.0

--- version.go
+++ version.go
@@ -7,8 +7,8 @@ import (
 	"github.com/fatih/color"
 )
 
-const Version string = "0.2.2"
-const BuildDate string = "2024-08-08"
+const Version string = "0.3.0"
+const BuildDate string = "2024-10-17"
 
 func cliCommandDisplayHelp(args []string) {
 	displayVersion := StringInSlice("-v", args[1:]) || StringInSlice("--version", args[1:])
